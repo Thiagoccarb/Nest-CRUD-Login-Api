8d5a082bee1039f355ebc82c5dfff5f12951ca8a
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-07-26 14:59:40 -0300

    Imports JwtModule in user.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from 'src/entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User]), JwtModule.register({})],
   controllers: [UsersController],
   providers: [UsersService],
 })

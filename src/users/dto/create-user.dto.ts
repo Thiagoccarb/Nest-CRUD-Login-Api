8014cee5389aa0fc415965096e1cb115249e9efa
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-07-25 22:29:11 -0300

    Implements class-validator logic in create-user-dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,12 @@
+import { IsNotEmpty, IsString, Min } from 'class-validator';
+
 export class CreateUserDto {
+  @Min(3)
+  @IsString()
+  @IsNotEmpty()
   readonly username: string;
-  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly hash: string;
 }

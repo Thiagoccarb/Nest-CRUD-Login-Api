b19f4e58b186427786efe93af356d44a7e906c2b
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-07-25 23:30:56 -0300

    Removes id column from user.entity.ts

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -2,20 +2,13 @@ import {
   Column,
   Model,
   Table,
-  PrimaryKey,
   AllowNull,
   Unique,
   DataType,
-  AutoIncrement,
 } from 'sequelize-typescript';
 
 @Table
 export class User extends Model {
-  @PrimaryKey
-  @AutoIncrement
-  @Column(DataType.STRING)
-  id: string;
-
   @AllowNull(false)
   @Unique(true)
   @Column(DataType.STRING)

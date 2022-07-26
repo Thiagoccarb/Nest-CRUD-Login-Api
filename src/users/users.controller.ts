1b1af6c6483ff35aa8248d056100bad36ffa5410
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-07-25 23:33:21 -0300

    Implements create method in users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import * as argon from 'argon2';
+
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -7,8 +9,12 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() user: CreateUserDto) {
-    return this.usersService.create(user);
+  async create(@Body() user: CreateUserDto) {
+    const hash = await argon.hash(user.password);
+    return this.usersService.create({
+      email: user.email,
+      hash,
+    });
   }
 
   // @Get()

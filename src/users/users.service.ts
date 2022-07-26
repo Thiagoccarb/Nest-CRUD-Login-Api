bd5499c3ff8bf9c6f17666f65c8971f86d3151ea
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-07-26 12:04:17 -0300

    Implements signIn method in userService.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import * as argon from 'argon2';
 
 import { User } from 'src/entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +14,21 @@ export class UsersService {
 
   async create(user: Partial<User>) {
     return this.userModel.create({ ...user });
+  }
+
+  async signIn(user: CreateUserDto) {
+    const existingUser = await this.userModel.findOne({
+      where: { email: user.email },
+      raw: true,
+    });
+    const passwordMatches = await argon.verify(
+      existingUser?.hash || '',
+      user.password,
+    );
+
+    if (!existingUser || !passwordMatches) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+    return existingUser;
   }
 }

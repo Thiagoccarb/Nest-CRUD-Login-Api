20bd3f5434969224bcd769a8bb058d464782cb69
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-07-27 15:12:24 -0300

    Implements reminder.entity.ts

---
+++ src/entities/reminder.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  Model,
+  Table,
+  AllowNull,
+  DataType,
+  BelongsTo,
+  ForeignKey,
+} from 'sequelize-typescript';
+import { User } from './user.entity';
+
+@Table
+export class Reminder extends Model {
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  title: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  description: string;
+
+  @AllowNull(false)
+  @Column(DataType.BOOLEAN)
+  status: boolean;
+
+  @ForeignKey(() => User)
+  @AllowNull(false)
+  @Column(DataType.NUMBER)
+  userId: number;
+
+  @BelongsTo(() => User)
+  user: User;
+}

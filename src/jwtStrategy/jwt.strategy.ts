9c7781767cedc83c6a9dae728c4b6ca1f6804c8e
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-07-27 11:47:25 -0300

    Creates jwt.strategy.ts component

---
+++ src/jwtStrategy/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Constants } from 'src/enums/constant';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: config.get(Constants.jwtSecret),
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username };
+  }
+}

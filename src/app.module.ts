b4ea6dbc30ba94a7540bc136737a0203b1178502
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-02 15:48:11 -0300

    Implements dotenv logic in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
 
 import { User } from './entities/user.entity';
 import { Reminder } from './entities/reminder.entity';
 import { UsersModule } from './users/users.module';
 import { JwtStrategy } from './jwtStrategy/jwt.strategy';
 import { RemindersModule } from './reminders/reminders.module';
+
+dotenv.config();
 
 @Module({
   imports: [
@@ -16,7 +19,8 @@ import { RemindersModule } from './reminders/reminders.module';
       port: 3306,
       username: 'root',
       password: '12345',
-      database: 'fumico-dev',
+      database:
+        process.env.NODE_ENV === 'development' ? 'fumico-dev' : 'fumico-test',
       models: [User, Reminder],
       autoLoadModels: true,
       synchronize: true,

eb5bb3a133643e240cbcd4b963772de5c03d97bd
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-03 19:55:21 -0300

    Implements ApiProperty decorator in create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,14 @@
 import { IsNotEmpty, IsString, IsEmail, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @MinLength(6)
   @IsString()
   @IsNotEmpty()
   readonly password: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsEmail({}, { message: 'invalid e-mail format' })

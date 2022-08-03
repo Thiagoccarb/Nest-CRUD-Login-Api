aa77c61194e7762d96a97b1e2cc80abd5b396fd5
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-03 19:52:52 -0300

    Implements DocumentBuilder and SwaggerModule logic in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Demo Application')
+    .setDescription('Demo API Application')
+    .setVersion('v1')
+    .addTag('fumico-api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,

78c04e3c6a167d0e077a145f7a18f88f51e9322d
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-03 19:54:22 -0300

    Implements ApiProperty decorator in create-reminder.dto.ts

--- src/reminders/dto/create-reminder.dto.ts
+++ src/reminders/dto/create-reminder.dto.ts
@@ -1,11 +1,22 @@
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateReminderDto {
+  @ApiProperty({
+    minLength: 3,
+    type: 'string',
+    required: true,
+  })
   @MinLength(3)
   @IsString()
   @IsNotEmpty()
   readonly title: string;
 
+  @ApiProperty({
+    minLength: 10,
+    type: 'string',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(10)

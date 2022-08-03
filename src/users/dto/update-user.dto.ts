e9b15ebdfefc13cb3fedb8d619468fb0ccccad45
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-03 19:55:35 -0300

    Implements ApiProperty decorator in update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateUserDto {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   hash?: string;
+  @ApiProperty()
   email: string;
 }

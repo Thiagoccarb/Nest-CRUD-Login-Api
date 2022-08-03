00d40c34d5062044f61b0d3a704114df5b3d91b8
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-03 19:55:12 -0300

    Implements ApiProperty decorator in create-reminder.dto.ts

--- src/reminders/dto/update-reminder.dto.ts
+++ src/reminders/dto/update-reminder.dto.ts
@@ -1,5 +1,10 @@
 import { CreateReminderDto } from './create-reminder.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateReminderDto extends CreateReminderDto {
+  @ApiProperty({
+    type: 'number',
+    description: 'the id is required when updating a reminder',
+  })
   readonly id: number;
 }
